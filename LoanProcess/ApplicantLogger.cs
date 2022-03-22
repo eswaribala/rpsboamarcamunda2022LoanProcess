acec91689e5f126bdf57d2938230c4b48415710d
eswaribala <parameswaribala@gmail.com>
2022-03-22 10:04:48 +0530

    Add project files.

---
+++ LoanProcess/ApplicantLogger.cs
@@ -0,0 +1,21 @@
+﻿using CamundaClient.Dto;
+using CamundaClient.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanProcess
+{
+    [ExternalTaskTopic("logger")]
+    
+    public class ApplicantLogger : IExternalTaskAdapter
+    {
+        public void Execute(ExternalTask externalTask, ref Dictionary<string, object> resultVariables)
+        {
+            Console.WriteLine("Logged");
+            Console.WriteLine("First Name={0}",externalTask.Variables["firstName"].Value);
+        }
+    }
+}
