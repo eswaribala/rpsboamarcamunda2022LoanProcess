8231b63a7aa6956051727a88523e05f152065d4b
eswaribala <parameswaribala@gmail.com>
2022-03-22 13:28:36 +0530

    updated

---
+++ LoanProcess/DMNOutputTask.cs
@@ -0,0 +1,19 @@
+﻿using CamundaClient.Dto;
+using CamundaClient.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanProcess
+{
+    [ExternalTaskTopic("dmnoutput")]
+    internal class DMNOutputTask : IExternalTaskAdapter
+    {
+        public void Execute(ExternalTask externalTask, ref Dictionary<string, object> resultVariables)
+        {
+            Console.WriteLine(externalTask.Variables["decision"].Value);
+        }
+    }
+}
---
+++ LoanProcess/LoanAdviceTask.cs
@@ -0,0 +1,20 @@
+﻿using CamundaClient.Dto;
+using CamundaClient.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanProcess
+{
+
+    [ExternalTaskTopic("loanadvice")]
+    public class LoanAdviceTask : IExternalTaskAdapter
+    {
+        public void Execute(ExternalTask externalTask, ref Dictionary<string, object> resultVariables)
+        {
+            Console.WriteLine(externalTask.Variables["decision"].Value);
+        }
+    }
+}
