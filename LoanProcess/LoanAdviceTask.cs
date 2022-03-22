867eb2b7728ee177b5c1ee8f949bbcb540c37773
eswaribala <parameswaribala@gmail.com>
2022-03-22 15:35:35 +0530

    updated

--- LoanProcess/LoanAdviceTask.cs
+++ LoanProcess/LoanAdviceTask.cs
@@ -15,6 +15,7 @@ namespace LoanProcess
         public void Execute(ExternalTask externalTask, ref Dictionary<string, object> resultVariables)
         {
             Console.WriteLine("Loan Advice {0}",externalTask.Variables["decision"].Value);
+            resultVariables.Add("advice", "accepted");
         }
     }
 }
